e56e9a7f8feda241adc7f674693fcbf2bbaabf6b
Ratthawan Sermsuwannasuk <ratthawanser@mathregit.group>
2020-07-30 14:22:49 +0700

    update component

--- frontend/toys-are-not-us/src/app/app.module.ts
+++ frontend/toys-are-not-us/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { PaymentComponent } from './payment/payment.component';
+import { ThanksYouComponent } from './thanks-you/thanks-you.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ProductDetailComponent,
+    ShoppingCartComponent,
+    ShippingComponent,
+    PaymentComponent,
+    ThanksYouComponent
   ],
   imports: [
     BrowserModule,
