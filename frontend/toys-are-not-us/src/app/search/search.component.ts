3f5442011d497ed5819e64633378c9fafd604f9f
Anthika Jirattananon <anthikajir@mathregit.group>
2020-07-30 15:14:51 +0700

    add service and product

--- frontend/toys-are-not-us/src/app/search/search.component.ts
+++ frontend/toys-are-not-us/src/app/search/search.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Product, allProduct } from '../model/product';
+import { ProductService } from '../service/product.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
+  products: Product[];
+  filterData: string = '';
 
-  constructor() { }
+  constructor(public service: ProductService) {}
 
   ngOnInit(): void {
+    //this.products = this.service.getAllProduct();
+    this.getAll();
   }
-
+  getAll(): void {
+    this.service.getAllProduct().subscribe((products) => {
+      return (this.products = products);
+    });
+  }
 }
