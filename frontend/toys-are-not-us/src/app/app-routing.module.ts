e9b5af4beb7fca16c7801c74ec7760739734651b
Anthika Jirattananon <anthikajir@mathregit.group>
2020-07-31 12:15:19 +0700

    finish all html to template

--- frontend/toys-are-not-us/src/app/app-routing.module.ts
+++ frontend/toys-are-not-us/src/app/app-routing.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SearchComponent } from './search/search.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { PaymentComponent } from './payment/payment.component';
+import { ThanksYouComponent } from './thanks-you/thanks-you.component';
 
 const routes: Routes = [
   { path: 'search', component: SearchComponent },
@@ -9,6 +13,10 @@ const routes: Routes = [
     path: 'product/:toy_id',
     component: ProductDetailComponent,
   },
+  { path: 'shopping-cart/:toy_id', component: ShoppingCartComponent },
+  { path: 'shipping', component: ShippingComponent },
+  { path: 'payment', component: PaymentComponent },
+  { path: 'thankyou', component: ThanksYouComponent },
 ];
 
 @NgModule({
--- frontend/toys-are-not-us/src/app/shopping-cart/shopping-cart.component.ts
+++ frontend/toys-are-not-us/src/app/shopping-cart/shopping-cart.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../service/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from './../model/product';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.css']
+  styleUrls: ['./shopping-cart.component.css'],
 })
 export class ShoppingCartComponent implements OnInit {
+  constructor(public service: ProductService, private route: ActivatedRoute) {}
 
-  constructor() { }
+  product: Product;
 
   ngOnInit(): void {
+    const productId: string = this.route.snapshot.paramMap.get('toy_id');
+    this.service.getSpecificProduct(productId).subscribe((p) => {
+      this.product = p;
+    });
+    console.log(this.product);
   }
-
 }
