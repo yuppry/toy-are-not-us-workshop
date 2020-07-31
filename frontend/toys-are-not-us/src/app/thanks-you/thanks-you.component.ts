0faa520c5dcee7f7e587911f7921dc7b757a0a37
Anthika Jirattananon <anthikajir@mathregit.group>
2020-07-31 14:43:43 +0700

    edit small function

--- frontend/toys-are-not-us/src/app/thanks-you/thanks-you.component.ts
+++ frontend/toys-are-not-us/src/app/thanks-you/thanks-you.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-thanks-you',
   templateUrl: './thanks-you.component.html',
-  styleUrls: ['./thanks-you.component.css']
+  styleUrls: ['./thanks-you.component.css'],
+  providers: [DatePipe],
 })
 export class ThanksYouComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  // myDate = new Date();
+  constructor(private datePipe: DatePipe) {
+    // this.myDate = this.datePipe.transform(this.myDate, 'yyyyMMdd');
   }
-
+  ngOnInit(): void {}
 }
